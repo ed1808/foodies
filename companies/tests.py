5ba7a0bbb15a0fd2a15dcde0d556eb03818b24a0
Edward Rodríguez <erodriguez@comunike.com>
2024-10-13 14:44:28 -0500

    Add companies tests

--- companies/tests.py
+++ companies/tests.py
@@ -1,3 +1,75 @@
+from django.db.utils import IntegrityError
 from django.test import TestCase
 
-# Create your tests here.
+from faker import Faker
+from random import randint
+
+from document_types.models import DocumentType
+from .models import Company
+
+
+class CompaniesTest(TestCase):
+    def setUp(self) -> None:
+        self.faker = Faker("es_CO")
+        self.faker.seed_instance(randint(0, 9999))
+        self.document_type = DocumentType.objects.create(
+            name="Número de Identificación Tributaria", code="NIT"
+        )
+
+    def test_create_company_returns_a_company_instance(self):
+        company = Company.objects.create(
+            name=self.faker.company(),
+            email=self.faker.company_email(),
+            phone=self.faker.phone_number(),
+            document_number=self.faker.legal_person_nit(),
+            document_type=self.document_type,
+            address=self.faker.address(),
+            city=self.faker.city(),
+            country=self.faker.country(),
+        )
+
+        self.assertIsInstance(company, Company)
+
+    def test_create_company_without_name_raises_integrity_error(self):
+        with self.assertRaises(IntegrityError):
+            Company.objects.create(
+                name=None,
+                email=self.faker.company_email(),
+                phone=self.faker.phone_number(),
+                document_number=self.faker.legal_person_nit(),
+                document_type=self.document_type,
+                address=self.faker.address(),
+                city=self.faker.city(),
+                country=self.faker.country(),
+            )
+
+    def test_company_str_returns_name(self):
+        company = Company.objects.create(
+            name=self.faker.company(),
+            email=self.faker.company_email(),
+            phone=self.faker.phone_number(),
+            document_number=self.faker.legal_person_nit(),
+            document_type=self.document_type,
+            address=self.faker.address(),
+            city=self.faker.city(),
+            country=self.faker.country(),
+        )
+
+        company_name = str(company)
+
+        self.assertEqual(company.name, company_name)
+
+    def test_delete_document_type_raises_integrity_error(self):
+        with self.assertRaises(IntegrityError):
+            Company.objects.create(
+                name=self.faker.company(),
+                email=self.faker.company_email(),
+                phone=self.faker.phone_number(),
+                document_number=self.faker.legal_person_nit(),
+                document_type=self.document_type,
+                address=self.faker.address(),
+                city=self.faker.city(),
+                country=self.faker.country(),
+            )
+
+            self.document_type.delete()
